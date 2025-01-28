30814c08a3db86c6dcc76b80f318690db593d3e5
Harsh Yadav <harshdana1@gmail.com>
2025-01-28 12:12:50 +0000

    remove milvus db

--- backend/internal/vectordb/vectordb.go
+++
@@ -1,7 +0,0 @@
-package vectordb
-
-type VectorDB interface {
-	Connect() error
-	InsertEmbedding(id string, vector []float32) error
-	SearchSimilarEmbeddings(query []float32, k int) ([]string, error)
-}
